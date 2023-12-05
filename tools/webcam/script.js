6b32bc3c26cf380aa157090190e004544a7c6fde
Vivatsathorn Thitasirivit <vivatsathorn@outlook.co.th>
2023-12-05 11:53:07 +0700

    Webcam Tools

--- tools/webcam/script.js
+++ tools/webcam/script.js
@@ -3,6 +3,12 @@ const cameraSelect = document.getElementById('cameraSelect');
 const fullscreenBtn = document.getElementById('fullscreenBtn');
 
 const reloadCamera = function () {
+    navigator.mediaDevices.getUserMedia({video: true})
+        .then(function (r) {
+            console.log(r);
+        })
+        .catch(handleError);
+
     navigator.mediaDevices.enumerateDevices()
         .then(setCameraDropdown)
         .catch(handleError);
